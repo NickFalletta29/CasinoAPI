3717eeb2aa84a80830b19d65363f257c0ccea31d
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:24:24 -0500

    Update User.java

--- API/src/Model/User.java
+++ API/src/Model/User.java
@@ -1,4 +1,26 @@
 package Model;
 
+// user in the system, stores username and balance and retrieves or sets them
 public class User {
+    private String username;
+    private double balance;
+
+    public User(String username, double balance) {
+        this.username = username;
+        this.balance = balance;
+    }
+
+    // gets username and balance
+    public String getUsername() {
+        return username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // updates/sets the balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
