9d2f90d044322b53baf0ac22b41894f5dcea9430
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:31:31 -0500

    Update Account.java

--- API/src/Model/Account.java
+++ API/src/Model/Account.java
@@ -1,4 +1,15 @@
 package Model;
 
+// uses the user object as represented by an account in system
 public class Account {
+    private User user;
+
+    public Account(User user) {
+        this.user = user;
+    }
+
+    // returns the user within the account
+    public User getUser() {
+        return user;
+    }
 }
