406ffbbeeb41ba5d35468d50a4bc98ef25913106
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:32:30 -0500

    Update LimitEnforcement.java

--- API/src/Model/LimitEnforcement.java
+++ API/src/Model/LimitEnforcement.java
@@ -1,4 +1,8 @@
 package Model;
 
+// check if transaction exceeds the user spending limit
 public class LimitEnforcement {
+    public static boolean exceedsLimit(double amount, SpendingLimit limit) {
+        return amount > limit.getLimitAmount();
+    }
 }
