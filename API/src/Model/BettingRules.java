e009ae4e0c906308b7974f42b2e46986e4672bff
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:29:05 -0500

    Update BettingRules.java

--- API/src/Model/BettingRules.java
+++ API/src/Model/BettingRules.java
@@ -1,4 +1,8 @@
 package Model;
 
+// contains rules to determine if bet is allowed
 public class BettingRules {
+    public static boolean isBetAllowed(double amount, double balance) {
+        return amount <= balance;
+    }
 }
