fd4e68e838a5f544f81b90d4a94a51c8783646bf
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:26:08 -0500

    Update SpendingLimit.java

--- API/src/Model/SpendingLimit.java
+++ API/src/Model/SpendingLimit.java
@@ -1,4 +1,18 @@
 package Model;
 
+// spending limit that can be set and modified as needed
 public class SpendingLimit {
+    private double limitAmount;
+
+    public SpendingLimit(double limitAmount) {
+        this.limitAmount = limitAmount;
+    }
+
+    public double getLimitAmount() {
+        return limitAmount;
+    }
+
+    public void setLimitAmount(double limitAmount) {
+        this.limitAmount = limitAmount;
+    }
 }
