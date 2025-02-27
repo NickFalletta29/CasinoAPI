c5e387d1380b807bbce45d996dd4bf53d3ede64e
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:37:18 -0500

    Update TransactionController.java

--- API/src/Controller/TransactionController.java
+++ API/src/Controller/TransactionController.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import Model.Transaction;
+import Model.User;
+
+// processes transaction
 public class TransactionController {
+    public boolean processTransaction(User user, Transaction transaction) {
+        if (user.getBalance() >= transaction.getAmount()) {
+            user.setBalance(user.getBalance() - transaction.getAmount());
+            return true; // success
+        }
+        return false; // unsuccess
+    }
 }
