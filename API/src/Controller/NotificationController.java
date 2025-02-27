566c4a9d1cd7fbf523d6711c42582bae0a9d6117
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:39:02 -0500

    Update NotificationController.java

--- API/src/Controller/NotificationController.java
+++ API/src/Controller/NotificationController.java
@@ -1,4 +1,9 @@
 package Controller;
 
+// send a notification
 public class NotificationController {
+    public void sendNotification(String message) {
+        // for now just print it
+        System.out.println("Notification: " + message);
+    }
 }
