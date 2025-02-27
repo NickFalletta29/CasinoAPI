bff8c6c33e6676a636f285d725810805dfd830f6
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:27:56 -0500

    Update Transaction.java

--- API/src/Model/Transaction.java
+++ API/src/Model/Transaction.java
@@ -1,4 +1,20 @@
 package Model;
 
+// the financial transaction with amount plus type (like bet or deposit or voucher) 
 public class Transaction {
+    private double amount;
+    private String type;
+
+    public Transaction(double amount, String type) {
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
