dc3eb185fe810752a33e7fe28ba62c6a84ad6403
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:34:07 -0500

    Update SpendingLimitController.java

--- API/src/Controller/SpendingLimitController.java
+++ API/src/Controller/SpendingLimitController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import Model.SpendingLimit;
+import Model.User;
+
+// controller for setting a spending limit for user
 public class SpendingLimitController {
+    public void setLimit(User user, SpendingLimit limit, double amount) {
+        limit.setLimitAmount(amount);
+    }
 }
+
+package Controller;
