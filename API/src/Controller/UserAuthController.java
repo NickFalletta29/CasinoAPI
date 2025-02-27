8f896f45f13977c0ccd9a0fd72c91e8e6c22731b
Sam Bender <mipninjas@gmail.com>
2025-02-26 20:35:23 -0500

    Update UserAuthController.java

--- API/src/Controller/UserAuthController.java
+++ API/src/Controller/UserAuthController.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import Model.User;
+
+// handles user auth
 public class UserAuthController {
+    public boolean authenticate(String username, String password) {
+        // check if username and password are equal to placeholder vals
+        return username.equals("testUser") && password.equals("password123");
+    }
 }
