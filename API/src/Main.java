21cf8475a9e4955b1e1f499e7b6bdc0dc605c496
tay3434 <tns5371@psu.edu>
2025-03-09 15:39:29 -0400

    Update Main.java

--- API/src/Main.java
+++ API/src/Main.java
@@ -1,6 +1,9 @@
 import Controller.SpendingLimitController;
 import Model.SpendingLimit;
 import View.SpendingLimitView;
+import View.UserView;
+import Model.User;
+import Controller.UserAuthController;
 
 import javax.swing.*;
 
@@ -17,6 +20,18 @@ public class Main {
         frame.setSize(400, 300);
         frame.add(view);
         frame.setVisible(true);
+
+        User testUser = new User("1", "testUser", "password123", "test@example.com", 100.0);
+
+        UserView authView = new UserView();
+        UserAuthController authController = new UserAuthController(testUser, authView);
+
+        //testing authentication
+        System.out.println("Testing Authentication:");
+        System.out.println("Correct Login: " + authController.authenticate("testUser", "password123")); //supposed to return true
+        System.out.println("Incorrect Login: " + authController.authenticate("testUser33", "pass444")); //supposed to return false
     }
 }
+
+
 
