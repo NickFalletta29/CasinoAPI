0286c608bf99c61ffd53e6057aec40cf43c6de2d
tay3434 <tns5371@psu.edu>
2025-04-13 15:39:08 -0400

    Update Main.java

--- API/src/Main.java
+++ API/src/Main.java
@@ -1,3 +1,76 @@
+import Controller.NotificationController;
+import Model.SpendingLimit;
+import Model.Transaction;
+import Model.User;
+import View.LoginView;
+import View.SpendingLimitView;
+import View.UserView;
+import Controller.SpendingLimitController;
+import Controller.TransactionController;
+import Controller.UserAuthController;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        // User Authentication Test
+        User testUser = new User("1", "testUser", "password123", "test@example.com", 100.0, "Placeholder", "Placeholder");
+        LoginView loginView = new LoginView();
+        NotificationController notif = new NotificationController();
+
+        // Setting up Spending Limit UI
+        SpendingLimit model = new SpendingLimit(notif);
+        SpendingLimitView view = new SpendingLimitView();
+        String userId = "user123";
+
+        new SpendingLimitController(model, view, userId);
+
+        JFrame frame = new JFrame("Casino Spending Limit");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.add(view);
+        frame.setVisible(true);
+
+
+       // user authentication continued 
+        User user = new User(userId, userId, userId, userId, 0, userId, userId);
+        UserView authView = new UserView(user, notif);
+        UserAuthController authController = new UserAuthController(testUser, loginView);
+
+        System.out.println("Testing Authentication:");
+        System.out.println("Correct Login: " + authController.authenticate("testUser", "password123")); // expected to be true
+        System.out.println("Incorrect Login: " + authController.authenticate("testUser33", "pass444")); // expected to be false
+
+        // Transaction Testing
+        TransactionController transactionController = new TransactionController(model);
+
+
+        System.out.println("\nTesting Transactions: ");
+        System.out.println("\nUser's current balance: " + testUser.getBalance());
+        //deposit was suscessful, balance increases by 50
+        Transaction deposit = new Transaction("001", 50.0, "deposit");
+        boolean depositSuccess = transactionController.processTransaction(testUser, deposit);
+        System.out.println("Deposit Transaction Success: " + depositSuccess + " | New Balance: " + testUser.getBalance());
+
+        //bet placed successfully, balance refelects that
+        Transaction bet = new Transaction("002", 30.0, "bet");
+        boolean betSuccess = transactionController.processTransaction(testUser, bet);
+        System.out.println("Bet Transaction Success: " + betSuccess + " | New Balance: " + testUser.getBalance());
+
+        //bet transaction failed balance stays the same
+        Transaction failedBet = new Transaction("003", 200.0, "bet");
+        boolean failedBetSuccess = transactionController.processTransaction(testUser, failedBet);
+        System.out.println("Bet Transaction Success: " + failedBetSuccess + " | New Balance: " + testUser.getBalance());
+
+        //withdrawl unsucessful balance stays the same
+        Transaction invalidTransaction = new Transaction("004", -10.0, "withdrawl");
+        boolean invalidSuccess = transactionController.processTransaction(testUser, invalidTransaction);
+        System.out.println("Withdrawl Transaction Success: " + invalidSuccess + " | New Balance: " + testUser.getBalance());
+    }
+}
+
+
+    
 // import View.SpendingLimitView;
 // import View.UserView;
 // import Model.User;
